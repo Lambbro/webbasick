bc96e64b579385bccd17e01234cddeed035709b4
Lamb <100024864+Lambbro@users.noreply.github.com>
2023-03-28 19:34:18 +0700

    Update XuLyPost.aspx.cs

--- TH/Buoi5/Bai9/srcFolder/Post/XuLyPost.aspx.cs
+++ TH/Buoi5/Bai9/srcFolder/Post/XuLyPost.aspx.cs
@@ -10,15 +10,15 @@ namespace Bai9.srcFolder.Post
     public partial class XuLyPost : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            String cpu =
+        {//Tạo biến chuỗi cpu lấy data từ input thông qua atribute name (ko phải id)
+            String cpu = //Với post thì dùng Request.Form (Method post sẽ nối data vào HTTP request body và Request.Form sẽ trả về dữ liệu được đưa HTTP request body)
                 "<h2>CPU</h2>" +
                 "<br>Tên VXL: " + Request.Form["cpuName"] +
                 "<br>Hãng: " + Request.Form["cpuFirm"] +
                 "<br>Ngày SX: " + Request.Form["cpuDate"] +
                 "<br>Giá: " + Request.Form["cpuPrice"];
-            Response.Write(cpu);
-            Response.End();
+            Response.Write(cpu);//Hiện chuỗi cpu lên web
+            Response.End();//Kết thúc lệnh
         }
     }
 }
