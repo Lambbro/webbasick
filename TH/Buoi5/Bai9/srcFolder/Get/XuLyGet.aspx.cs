151e681e3d5098937c3807f6072f5660d1499659
Lamb <100024864+Lambbro@users.noreply.github.com>
2023-03-28 19:03:07 +0700

    Add files via upload

---
+++ TH/Buoi5/Bai9/srcFolder/Get/XuLyGet.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bai9.srcFolder.Get
+{
+    public partial class XuLyGet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String cpu =
+                "<h2>CPU</h2>" +
+                "<br>Tên VXL: " + Request.QueryString["cpuName"] +
+                "<br>Hãng: " + Request.QueryString["cpuFirm"] +
+                "<br>Ngày SX: " + Request.QueryString["cpuDate"] +
+                "<br>Giá: " + Request.QueryString["cpuPrice"];
+            Response.Write(cpu);
+            Response.End();
+        }
+    }
+}
---
+++ TH/Buoi5/Bai9/srcFolder/Post/XuLyPost.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bai9.srcFolder.Post
+{
+    public partial class XuLyPost : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String cpu =
+                "<h2>CPU</h2>" +
+                "<br>Tên VXL: " + Request.Form["cpuName"] +
+                "<br>Hãng: " + Request.Form["cpuFirm"] +
+                "<br>Ngày SX: " + Request.Form["cpuDate"] +
+                "<br>Giá: " + Request.Form["cpuPrice"];
+            Response.Write(cpu);
+            Response.End();
+        }
+    }
+}
