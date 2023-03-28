f4c1215a6d29ccd16a290360d1305ccf522a0064
Lamb <100024864+Lambbro@users.noreply.github.com>
2023-03-28 19:29:37 +0700

    Update XuLyGet.aspx.cs

--- TH/Buoi5/Bai9/srcFolder/Get/XuLyGet.aspx.cs
+++ TH/Buoi5/Bai9/srcFolder/Get/XuLyGet.aspx.cs
@@ -10,15 +10,15 @@ namespace Bai9.srcFolder.Get
     public partial class XuLyGet : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            String cpu =
+        {   //Tạo biến chuỗi cpu lấy data từ input thông qua atribute name (ko phải id)
+            String cpu = //Với phương thức get thì sử dụng QueryString (Get gửi data thông qua url và QueryString chính là phần url chứa data đc gửi đi đó)
                 "<h2>CPU</h2>" +
                 "<br>Tên VXL: " + Request.QueryString["cpuName"] +
                 "<br>Hãng: " + Request.QueryString["cpuFirm"] +
                 "<br>Ngày SX: " + Request.QueryString["cpuDate"] +
                 "<br>Giá: " + Request.QueryString["cpuPrice"];
-            Response.Write(cpu);
-            Response.End();
+            Response.Write(cpu);//Hiện chuỗi cpu lên web
+            Response.End();//Kết thúc lệnh
         }
     }
 }
