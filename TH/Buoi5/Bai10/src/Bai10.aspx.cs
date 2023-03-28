d6ef546376499f0300b0c926cf1e6b313cd80b56
Lamb <100024864+Lambbro@users.noreply.github.com>
2023-03-28 23:38:08 +0700

    Add files via upload

---
+++ TH/Buoi5/Bai10/src/Bai10.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bai10.src
+{
+    public partial class Bai10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String txtNoidung = Request.Form["txtNoidung"];
+            Response.Write("Content: " + txtNoidung);
+
+            HttpPostedFile file = Request.Files["cFile"];//Lấy file đc post
+            String cF = Request.Form["cFile"];
+            if (file == null) { Response.Write("<br>Lỗi file: " + cF); }//File khác null thì thông báo lỗi
+            else
+            {
+                file.SaveAs(Server.MapPath("../App_Data\\" + file.FileName));//Lưu file
+            }
+            Response.End();
+        }
+    }
+}
