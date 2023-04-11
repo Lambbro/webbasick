cc42a4a1947a77674d88441019b01ac185b12921
Lamb <100024864+Lambbro@users.noreply.github.com>
2023-04-11 10:48:29 +0700

    Add files via upload

--- TH/BTDK/BTDK.js
+++ TH/BTDK/BTDK.js
@@ -7,35 +7,48 @@ let pTable = document.getElementById("productTable");
 let selection = document.getElementById("sortSelection");
 let inpName = document.getElementById("inpName");
 let inpPrice = document.getElementById("inpPrice");
+let btnCancel = document.getElementById("btnCancel");
 
 function btnHandler () {
     if (inputField.style.display == 'none') {
         inputField.style.display = 'block';
         listField.style.display = 'none';
+        btnCancel.style.display = "inline";
     } else {
+        if (inpName.value.trim() == "") {
+            alert("Mời nhập tên sản phẩm");
+            inpName.focus();
+            return;
+        }
+        if (inpPrice.value <= 0) {
+            alert("Mời nhập giá sản phẩm")
+            inpPrice.focus();
+            return;
+        }
+        for (i=0;i<list.length;i++) {
+            if (inpName.value.trim().toLowerCase() == list[i].name.trim().toLowerCase()) {
+                alert ("Đã có sản phẩm này");
+                return;
+            }
+        }
         addItem();
-        deleteTable();
-        addToScreen();
-        inputField.style.display = 'none';
-        listField.style.display = 'block';
+        sort();
+        cancel();
     }
 }
 
+function cancel() {
+    btnCancel.style.display = "none";
+    inpName.value = "";
+    inpPrice.value = "";
+    inputField.style.display = 'none';
+    listField.style.display = 'block';
+}
+
 function addItem () {
-    if (inpName.value.trim() == "" || inpPrice.value == "" || inpPrice <=0 ) {
-        return;
-    }
     let newItem = {
         name: inpName.value.trim(),
         price: inpPrice.value
-    }
-    inpName.value = "";
-    inpPrice.value = "";
-    for (i=0;i<list.length;i++) {
-        if (newItem.name.trim().toLowerCase() == list[i].name.trim().toLowerCase()) {
-            alert ("Đã có sản phẩm này");
-            return;
-        }
     }
     list.push(newItem);
 }
@@ -48,7 +61,7 @@ function addToScreen () {
 
         cell1.classList.add("productName");
         cell1.classList.add("paddingBox");
-        cell2.classList.add("productName");
+        cell2.classList.add("productPrice");
         cell2.classList.add("paddingBox");
 
         cell1.innerHTML = list[i].name;
